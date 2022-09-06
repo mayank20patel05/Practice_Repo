4a76b42e4507b5ad3cedd0816e511147b5215d57
Mayank Patel <mayank20patel05@gmail.com>
2022-09-06 19:00:38 -0400

    added employee class

---
+++ src/demo/Employee.java
@@ -0,0 +1,47 @@
+package demo;
+
+public class Employee {
+    private long id;
+    private String name;
+    private String email;
+    private long phone;
+
+    public Employee(long id, String name, String email, long phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+}
